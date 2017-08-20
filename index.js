29b2d34db6886aa3186c4d9a7c933811e813588c
Jesse Shaw <jdshaw1987@gmail.com>
2017-08-20 15:50:45 -0400

    Succesfully called last.fm JSON and displayed data on the page

---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+Application name	Last.fm Music Rating Service
+API key	48a68fa743a6e709380166a2342c0c27
+Shared secret	23a2a34d9f04f79a30f4c01246f2e68f
+Registered to	bquietndrive87
+*/
+
+var key = "48a68fa743a6e709380166a2342c0c27";
+var url = "http://ws.audioscrobbler.com/2.0/";
+
+function getApiData(searchTerm) {
+    console.log(`Function has been executed. You have searched for ${searchTerm}`);
+    var searchUrl = `${url}?method=artist.gettopalbums&artist=${searchTerm}&api_key=${key}&format=json`
+    const settings = {
+        artist: `${searchTerm}`,
+        api_key: key,
+        method: "artist.gettopalbums",
+        format: "json"
+    }
+    $.getJSON(url, settings, function(result){
+        $.each(result, function(i, field){
+            for (var i = 0; i < 5; i++) {
+                result.preventDefault;
+                $(".js-search-results").append(`<h1>${result.topalbums.album[i].name}</h1>`)
+            }
+        })
+    });
+}
+
+
+$("form").on("submit", function(e) {
+    e.preventDefault();
+    var name = $(".userinput").val();
+    console.log(name);
+    getApiData(name);
+})
