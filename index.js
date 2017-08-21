4cbcd5882cadf26a5c20e7efd5df57be5e1d472a
Jesse Shaw <jdshaw1987@gmail.com>
2017-08-20 21:24:50 -0400

    Updated (1) Styling, (2) Clear results after a new input is entered

--- index.js
+++ index.js
@@ -9,19 +9,17 @@ var key = "48a68fa743a6e709380166a2342c0c27";
 var url = "https://ws.audioscrobbler.com/2.0/";
 
 function getApiData(searchTerm) {
-    console.log(`Function has been executed. You have searched for ${searchTerm}`);
-    var searchUrl = `${url}?method=artist.gettopalbums&artist=${searchTerm}&api_key=${key}&format=json`
     const settings = {
         artist: `${searchTerm}`,
         api_key: key,
         method: "artist.gettopalbums",
-        format: "json"
+        format: "json",
+        autocorrect: 1
     }
     $.getJSON(url, settings, function(result){
         $.each(result, function(i, field){
             for (var i = 0; i < 3; i++) {
-                $(".js-search-results").append(`<h1>${i + 1}. ${result.topalbums.album[i].name}</h1>`);
-                $(".js-search-results").append(`<img src="${result.topalbums.album[i].image[3]["#text"]}">`);
+                $(".js-search-results").append(`<div class="searchresult"><h2>${i + 1}. ${result.topalbums.album[i].name}</h2><img src="${result.topalbums.album[i].image[3]["#text"]}" alt="albumcover"></div>`);
             }
         })
     });
@@ -30,10 +28,10 @@ function getApiData(searchTerm) {
 function watchSubmit() {
     $("form").on("submit", function(e) {
         e.preventDefault();
+        $(".searchresult").remove();
         var name = $(".userinput").val();
-        console.log(name);
         getApiData(name);
-    })    
+    })
 }
 
 $(watchSubmit);
